44430acab28b15e34994c490eeb0456a875f6bfd
Vinicius <vsampaio@ice.ufjf.br>
2018-05-23 15:36:35 -0300

    testes com a Splay

--- main.cpp
+++ main.cpp
@@ -13,6 +13,14 @@ int main()
         cout<<dados[i].getQuestionID()<<" "<<dados[i].getScore()<<endl;
         arvore.inserir(dados[i].getQuestionID(),dados[i].getUserID(),dados[i].getDate(),dados[i].getScore(),dados[i].getTitle());
     }
+    cout<<endl;
     arvore.imprime();
+    cout<<endl;
+    arvore.busca(dados[0].getQuestionID());
+    arvore.imprime();
+    cout<<endl;
+    arvore.deletar(dados[0].getQuestionID());
+    arvore.imprime();
+    cout<<endl;
     delete [] dados;
 }
